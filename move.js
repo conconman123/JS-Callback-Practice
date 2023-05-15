527cdada7e00b03dc4df7743816ce5e2792f5d37
Conor Panuccio <conorpanu@hotmail.com>
2023-05-15 17:25:50 -0400

    added boundrys

--- move.js
+++ move.js
@@ -13,16 +13,16 @@ function move(element) {
         element.style.bottom = y + 'px'
         
         function moveCharacter(){
-            if(direction === 'west'){
+            if(direction === 'west' && x > 0){
                 x-=1
             }
-            if(direction === 'north'){
+            if(direction === 'north' && y < ((window.innerHeight * .7) - 30)){
                 y+=1
             }
-            if(direction === 'east')[
+            if(direction === 'east' && x < (window.innerWidth - 55))[
                 x+=1
             ]
-            if(direction === 'south'){
+            if(direction === 'south' && y > 100){
                 y-=1
             }
             element.style.left = x + 'px'
